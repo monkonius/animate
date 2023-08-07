64c134abfc13774d680dcf2b0769c60366801557
monkonius <65208909+monkonius@users.noreply.github.com>
2023-08-07 18:36:08 +0800

    Place like and review functionality in separate files

---
+++ reviews/static/reviews/like.js
@@ -0,0 +1,35 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function likeReview(node) {
+    const likeCount = node.children[1];
+    node.classList.toggle('liked');
+
+    const review = node.parentElement;
+    let count = Number(likeCount.innerHTML)
+
+    const csrftoken = getCookie('csrftoken');
+    fetch(`/like/${review.id}`, {
+        method: 'POST',
+        headers: { 'X-CSRFToken': csrftoken },
+        mode: 'same-origin'
+    })
+        .then(response => response.json())
+        .then(data => {
+            data.created ? count++ : count--;
+            likeCount.innerHTML = count;
+        })
+}
