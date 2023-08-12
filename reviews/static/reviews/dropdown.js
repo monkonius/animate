e515fdd904f02468ed71d1ae048d0e62e4d6dd3e
monkonius <65208909+monkonius@users.noreply.github.com>
2023-08-12 12:49:17 +0800

    Make the dropdown list disappear when clicking on anything else

--- reviews/static/reviews/dropdown.js
+++ reviews/static/reviews/dropdown.js
@@ -2,3 +2,14 @@ function showContent(button) {
     dropdownContent = button.nextElementSibling;
     dropdownContent.classList.toggle('show');
 }
+
+window.onclick = (event) => {
+    if (!event.target.matches('.dropdown')) {
+        const dropdownContent = document.querySelector('.dropdown-content');
+        if (dropdownContent) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    }
+}
