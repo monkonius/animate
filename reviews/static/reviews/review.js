5e2c186a3bb25be4de4134c32b453891839de45c
monkonius <65208909+monkonius@users.noreply.github.com>
2023-08-23 15:14:34 +0800

    Move sort form submit to one file

--- reviews/static/reviews/review.js
+++ reviews/static/reviews/review.js
@@ -13,3 +13,11 @@ if (toggleButton && cancelButton) {
         })
     })
 }
+
+const sort = document.getElementById('sort-by');
+if (sort) {
+    sort.addEventListener('change', (elem) => {
+        const select = elem.target;
+        select.form.submit();
+    })
+}
--- reviews/static/reviews/sort.js
+++
@@ -1,7 +0,0 @@
-const sort = document.getElementById('sort-by');
-if (sort) {
-    sort.addEventListener('change', (elem) => {
-        const select = elem.target;
-        select.form.submit();
-    }) 
-}
