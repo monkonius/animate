3b55b264ad778187b6a582bce04477807d53e0ef
monkonius <65208909+monkonius@users.noreply.github.com>
2023-08-23 13:55:19 +0800

    Add event listener with JS instead of using inline event listener

--- reviews/static/reviews/review.js
+++ reviews/static/reviews/review.js
@@ -1,7 +1,15 @@
-function toggleReview() {
-    const reviewForm = document.querySelector('.review-form');
-    reviewForm.classList.toggle('show');
-    
-    const toggleButton = document.getElementById('review-toggle');
-    toggleButton.classList.toggle('hide');
+const toggleButton = document.getElementById('review-toggle');
+const cancelButton = document.getElementById('cancel');
+
+if (toggleButton && cancelButton) {
+    const buttons = [];
+    buttons.push(toggleButton, cancelButton);
+    buttons.forEach(elem => {
+        elem.addEventListener('click', () => {
+            const reviewForm = document.querySelector('.review-form');
+            reviewForm.classList.toggle('show');
+
+            toggleButton.classList.toggle('hide');
+        })
+    })
 }
