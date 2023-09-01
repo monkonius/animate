d1a5af699a95aa80462469b1d5fd51b3cb057447
monkonius <65208909+monkonius@users.noreply.github.com>
2023-09-01 18:49:46 +0800

    Create tests for review model

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import Review, User
 
-# Create your tests here.
+
+class ReviewTestCase(TestCase):
+    def setUp(self):
+        testuser = User.objects.create(
+            username='testuser', email='testuser@email.com', password='password'
+        )
+
+        Review.objects.create(
+            anime_id=1, content='test', recommendation='RE', author=testuser
+        )
+        Review.objects.create(
+            anime_id=20, content='test', recommendation='MF', author=testuser
+        )
+
+    def test_review_count(self):
+        user = User.objects.get(username='testuser')
+        self.assertEqual(user.reviews.count(), 2)
+
+    def test_review_content(self):
+        review = Review.objects.get(anime_id=1)
+        self.assertEqual(review.content, 'test')
+
+    def test_review_recommendation(self):
+        review = Review.objects.get(anime_id=20)
+        self.assertEqual(review.recommendation, 'MF')
+    
+    def test_review_string_rep(self):
+        review = Review.objects.get(anime_id=1)
+        string = 'Review on anime no. 1 by testuser'
+        self.assertEqual(str(review), string)
