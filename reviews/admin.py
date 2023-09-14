924645c045a090c3f48b0daef45facdafb54795e
monkonius <65208909+monkonius@users.noreply.github.com>
2023-09-14 16:46:37 +0800

    Register models to admin application

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Review, Like, Dislike
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Like)
+admin.site.register(Dislike)
